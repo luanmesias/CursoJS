51f65fb6aa46a6bf16d494d8213b3d8a1b99829a
Luan Messias <luanmesias1211@gmail.com>
2026-01-11 05:40:42 +0000

    Atualização

--- aula7/index.js
+++ aula7/index.js
@@ -2,7 +2,7 @@ const nome = 'João';
 
 console.log(nome);
 
-const primeiroNumero = '5';
+const primeiroNumero = Number('5');
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
 const resultadoDuplicado = resultado * 2;
